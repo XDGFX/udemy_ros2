bbbbce0257e72ad3240585a1e577bd32f99f311a
Callum <callum.morrison@mac.com>
2021-06-02 13:57:25 +0100

    first commit

---
+++ scripts/publisher.py
@@ -0,0 +1,12 @@
+import rclpy
+
+
+def main():
+    # Initialise ROS client library
+    # It can take commandline arguments or a context name
+    # as input parameters, which we will not use currently.
+    rclpy.init()
+
+
+if __name__ == "__main__":
+    main()
